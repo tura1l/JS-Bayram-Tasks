ba77088ad80d996fbecd67e543812a20d82da13b
tura1l <7nfphb0@code.edu.az>
2023-04-25 01:51:55 +0400

    JS

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+//Taks a
+// function arichmatics(x,y,z) {
+//     var x;
+//     var y;
+//     switch (z) {
+//         case "+" :
+//             alert(x+y)
+//             break;
+//         case "-" :
+//             alert(x-y)
+//             break;
+//         case "*" :
+//             alert(x*y)
+//             break;
+//         case "/" :
+//              alert(x/y)
+//              break;
+//         default: alert("Duzgun emeliyyat daxil edin")
+//             break;
+//     }
+// }
+// alert(arichmatics(Number(prompt("Birinci ededi daxil edin")),Number(prompt("Ikinci ededi daxil edin")),prompt("Emel daxil edin")));
+
+
+//Task b
+// 
+
+//Task c
+// function bolunenlerinSayi(num){
+
+//     var say = 0;
+
+//     if(num > 0 && num < 50){
+//         for(let i = 1; i < num; i++){
+//             if(i % 3 === 0){
+//                 say++;
+//             }
+//         }
+//     }
+//     else if(num > 50 && num < 100){
+//         for(let i = 1; i < num; i++){
+//             if(i % 5 === 0){
+//                 say++;
+//             }
+//         }
+//     }
+//     else {
+//         for(let i = 1; i < num; i++){
+//             if(i % 8 === 0){
+//                 say++;
+//             }
+//         }
+//     }
+//     return say;
+// }
+// alert(bolunenlerinSayi(+prompt("Eded daxil edin: ")));
+
